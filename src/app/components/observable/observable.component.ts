ec36cabdeff0ef5bd595946555fe10b3804220dc
book <bookgvi@gmail.com>
2020-01-30 01:51:21 +0300

    Отслеживание переменной спомощью Observable RxJS

--- src/app/components/observable/observable.component.ts
+++ src/app/components/observable/observable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
@@ -7,19 +8,34 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class ObservableComponent implements OnInit, OnDestroy {
   public variable: number = 0;
+  private observableInstance: Observable<number>;
+  private subscribtion: Subscription;
+  private subscriber: Subscriber<number>;
 
   constructor() {
   }
 
-  public changeVariable(): number {
+  public changeVariable(self): number {
     this.variable = Math.ceil(Math.random() * 256);
+    this.subscriber.next(this.variable);
     return this.variable;
   }
 
+  private observable(): Observable<number> {
+    return new Observable((observer: Subscriber<number>) => {
+      this.subscriber = observer;
+    });
+  }
+
   ngOnInit() {
+    this.observableInstance = this.observable();
+    this.subscribtion = this.observableInstance.subscribe((val: number) => {
+      console.log('QQQ', val);
+    });
   }
 
   ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
   }
 
 }
