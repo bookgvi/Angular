3a9036ae4f625bed35fdbb26dc4ee182895d096e
book <bookgvi@gmail.com>
2020-01-29 23:22:13 +0300

    Начальная структура проекта

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './router/app-routing.module';
 import { AppComponent } from './app.component';
+import { ObservableComponent } from './components/observable/observable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservableComponent
   ],
   imports: [
     BrowserModule,
--- src/app/router/app-routing.module.ts
+++ src/app/router/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ObservableComponent } from '../components/observable/observable.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'observe', component: ObservableComponent },
+  { path: '**', component: ObservableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
