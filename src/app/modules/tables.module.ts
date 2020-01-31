94bdbfe5109a662e2b234e90c542b3facbba437c
Виатлий <vitaliy.guselnikov@ucann.ru>
2020-01-31 13:29:08 +0300

    URBAX-5: добавлены компоненты для форм и графиков, моаршрутизация

--- src/app/modules/tables.module.ts
+++ src/app/modules/tables.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { TablesComponent } from '../components/tables/tables.component';
 import { BootstrapTableComponent } from '../components/tables/bootstrap-table/bootstrap-table.component';
 
+import { FormsComponent } from '../components/forms/forms.component';
+
+import { ChartsComponent } from '../components/charts/charts.component';
+
 @NgModule({
   declarations: [
     TablesComponent,
-    BootstrapTableComponent
+    BootstrapTableComponent,
+    FormsComponent,
+    ChartsComponent
   ],
   imports: []
 })
--- src/app/router/app-routing.module.ts
+++ src/app/router/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TablesComponent } from '../components/tables/tables.component';
+import { FormsComponent } from '../components/forms/forms.component';
+import { ChartsComponent } from '../components/charts/charts.component';
 
 
 const routes: Routes = [
   { path: 'tables', component: TablesComponent },
+  { path: 'forms', component: FormsComponent },
+  { path: 'charts', component: ChartsComponent },
   { path: '**', redirectTo: 'tables' }
 ];
 
