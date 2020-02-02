b67300af6651d988eb5b7cfb2b17515269c7183d
book <bookgvi@gmail.com>
2020-02-02 12:39:28 +0300

    Графики

--- src/app/components/charts/chart-pie/chart-pie.component.ts
+++ src/app/components/charts/chart-pie/chart-pie.component.ts
@@ -10,30 +10,21 @@ export class ChartPieComponent {
 
   constructor() {
     this.data = {
-      datasets: [{
-        data: [
-          11,
-          16,
-          7,
-          3,
-          14
-        ],
-        backgroundColor: [
-          "#FF6384",
-          "#4BC0C0",
-          "#FFCE56",
-          "#E7E9ED",
-          "#36A2EB"
-        ],
-        label: 'My dataset'
-      }],
-      labels: [
-        "Red",
-        "Green",
-        "Yellow",
-        "Grey",
-        "Blue"
-      ]
-    }
+      labels: ['A','B','C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+    };
   }
 }
