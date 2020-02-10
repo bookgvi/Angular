cc2d72d56c6225a8e31973918a0d509067a71f0d
book <bookgvi@gmail.com>
2020-02-10 18:29:19 +0300

    RxJS: event loop and scheduler. 4 types of events

--- src/app/components/rxjs-sheduler/rxjs-sheduler.component.ts
+++ src/app/components/rxjs-sheduler/rxjs-sheduler.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { startWith } from 'rxjs/operators';
+import { queue } from 'rxjs/internal/scheduler/queue';
+import { asap } from 'rxjs/internal/scheduler/asap';
+import { async } from 'rxjs/internal/scheduler/async';
+import { animationFrame } from 'rxjs/internal/scheduler/animationFrame';
 
 @Component({
   selector: 'app-rxjs-sheduler',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rxjs-sheduler.component.css']
 })
 export class RxjsShedulerComponent implements OnInit {
+  private dummyArray: string[];
 
-  constructor() { }
+  constructor() {
+    this.dummyArray = ['a', 'b', 'c', 'd'];
+  }
 
   ngOnInit() {
+    from(this.dummyArray).pipe(
+      startWith('Queue scheduler...', queue)
+    ).subscribe({
+      next(v: string) {
+        console.log(v)
+      }
+    });
+    from(this.dummyArray).pipe(
+      startWith('ASAP scheduler...', asap)
+    ).subscribe({
+      next(v: string) {
+        console.log(v)
+      }
+    });
+    from(this.dummyArray).pipe(
+      startWith('Async scheduler...', async)
+    ).subscribe({
+      next(v: string) {
+        console.log(v)
+      }
+    });
+    from(this.dummyArray).pipe(
+      startWith('Animation frame scheduler...', animationFrame)
+    ).subscribe({
+      next(v: string) {
+        console.log(v)
+      }
+    });
   }
 
 }
